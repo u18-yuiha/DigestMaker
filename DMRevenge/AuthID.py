4236b33e04dcb692dbc04297bc9ef2e9703db2df
u18-yuiha <oochoko.2013@gmail.com>
2020-07-14 12:16:25 +0900

    test of authenticaiotn

--- DMRevenge/AuthID.py
+++ DMRevenge/AuthID.py
@@ -1,16 +1,24 @@
 import socket
 import uuid
+import sys
+import os
 import COMPONENT.BasicErrorComponent as BEC
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
 def register_ID():
     s = uuid.getnode()
     s = str(s)
     try:
-        with open('ID.text', mode='x') as f:
+        with open(resource_path('ID.text'), mode='x') as f:
             f.write(s)
     except FileExistsError:
         pass
 def authentication():
-    with open('ID.text') as f:
+    with open(resource_path('ID.text')) as f:
         s = f.read()
         s = int(s)
     auth_flag = True   
